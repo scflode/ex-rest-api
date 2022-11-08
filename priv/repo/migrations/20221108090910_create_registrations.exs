519dfad1e4af90971b4d35a181c2545dc9e99801
Flo Schuessel <mail@florian-schuessel.de>
2022-11-08 14:49:43 +0100

    Merge migrations

--- priv/repo/migrations/20221108090910_create_registrations.exs
+++ priv/repo/migrations/20221108090910_create_registrations.exs
@@ -4,6 +4,8 @@ defmodule RestApi.Repo.Migrations.CreateRegistrations do
   def change do
     create table(:registrations, primary_key: false) do
       add :id, :binary_id, primary_key: true
+      add :first_name, :string
+      add :last_name, :string
 
       timestamps()
     end
